5cc6ae196940cfa7c8665e51579bc8ac2176ea7c
Tah Ezekiel <tahfonda@gmail.com>
2022-12-12 17:29:18 -0800

    4 alphabte

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - print all letters except e and q
+*Return: always return the value 0
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter = 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
