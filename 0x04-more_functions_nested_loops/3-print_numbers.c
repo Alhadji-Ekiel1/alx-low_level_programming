10343f9a8cee4bf1b46951089d9b7dfeab239030
Tah Ezekiel <tahfonda@gmail.com>
2022-12-15 19:29:00 -0800

    print_numbers 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
-*print_most_numbers - prints numbers between 0 to 9
+*print_numbers - checks for checks for a digit (0 through 9).
 *Return: always returns 0
 */
-
-void print_most_numbers(void)
+void print_numbers(void)
 {
-	int ch;
+	int c;
 
-	for (ch = 48; ch < 58; ch++)
+	for (c = 48; c < 58; c++)
 	{
-		_putchar(ch);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
