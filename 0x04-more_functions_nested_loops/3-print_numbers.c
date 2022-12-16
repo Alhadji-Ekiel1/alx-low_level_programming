82d74fa2df7ae0fc79ef6f9d27787385714e99f2
Tah Ezekiel <tahfonda@gmail.com>
2022-12-15 19:17:35 -0800

    3print 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
 *print_most_numbers - prints numbers between 0 to 9
-*(neither 2 nor 4).
-*Return: no return.
+*Return: always returns 0
 */
 
 void print_most_numbers(void)
@@ -11,8 +10,7 @@ void print_most_numbers(void)
 
 	for (ch = 48; ch < 58; ch++)
 	{
-		if (ch != 50 && ch != 52)
-			_putchar(ch);
+		_putchar(ch);
 	}
 	_putchar('\n');
 }
